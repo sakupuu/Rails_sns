509a1c49e838126f86f50ecffaf56638a9361376
sakupuu <saku.yuzu.113@gmail.com>
2021-04-23 22:24:36 +0900

    メールアドレスとパスワードでログインする機能を実装しました。ご確認お願いいたします。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    end
+  
+  
+  
+  
+  
+  # def after_sign_in_path_for(resource)
+  #   pages_show_path
+  # end
+
+  # private
+  # def sign_in_required
+  #     redirect_to new_user_session_url unless user_signed_in?
+  # end
+
+
 end
