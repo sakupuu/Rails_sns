12698a1ba6cad19bd211cae0b20240a23b702403
sakupuu <saku.yuzu.113@gmail.com>
2021-04-25 16:44:04 +0900

    お気に入り機能を追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'tweets#index'
   devise_for :users
-  resources :tweets  
+
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users
-  # root 'pages#index'
-  # get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
