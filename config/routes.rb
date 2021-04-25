82c0266166396a9a1d21286d4b09d29866080759
sakupuu <saku.yuzu.113@gmail.com>
2021-04-25 14:36:33 +0900

    ツイート投稿機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'tweets#index'
   devise_for :users
-  root 'pages#index'
-  get 'pages/show'
+  resources :tweets  
+  resources :users
+  # root 'pages#index'
+  # get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
