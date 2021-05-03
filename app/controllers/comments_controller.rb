9aa3b522e69f2410c6378921507a5c1e160ddb4c
sakupuu <saku.yuzu.113@gmail.com>
2021-05-03 19:09:37 +0900

    flash機能を編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,9 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     if @comment.save
       redirect_back(fallback_location: root_path)
+      flash[:success] = "コメントしました"
     else
-      flash[:success] = "コメントできませんでした"
+      flash[:alert] = "コメントできませんでした"
       redirect_back(fallback_location: root_path)
     end
   end
