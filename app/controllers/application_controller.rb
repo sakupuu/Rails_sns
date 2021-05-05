701e2867b07a249087a9fe15a7fc1e88886c8819
sakupuu <saku.yuzu.113@gmail.com>
2021-05-05 15:45:03 +0900

    エラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
   end
+  
+  def sign_in_required
+    redirect_to new_user_session_url unless user_signed_in?
+  end
 
 end
