7f15082d4d22744edd9aa0225909eb4aac0f6084
sakupuu <saku.yuzu.113@gmail.com>
2021-05-06 22:08:19 +0900

    インデント削除

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,9 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]  
+  before_action :authenticate_user!, except: [:index]
 
   def new
     @tweet = Tweet.new
-  end 
+  end
 
   def index
     @tweets = Tweet.all
@@ -18,8 +18,8 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)   
-    @tweet.user_id = current_user.id 
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
     @tweet.save
     redirect_to tweets_path
   end
@@ -44,6 +44,6 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:body) 
+    params.require(:tweet).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 
   resources :users do
     resource :relationships, only: [:create, :destroy]
-    get :follows, on: :member 
-    get :followers, on: :member 
+    get :follows, on: :member
+    get :followers, on: :member
   end
 
   resources :users
