2e0a4423f23308a8d9397eb4b97771a358e70dbd
sakupuu <saku.yuzu.113@gmail.com>
2021-05-07 21:26:02 +0900

    コメントアウト消去

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,9 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]  
+  before_action :authenticate_user!, except: [:index]
 
   def new
     @tweet = Tweet.new
-  end 
+  end
 
   def index
     @tweets = Tweet.all
@@ -13,14 +13,13 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
     @user = @tweet.user
     @comments = @tweet.comments
-    # @comment = @tweet.comments.build
     @tweetnew = Tweet.new
     @comment = Comment.new
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)   
-    @tweet.user_id = current_user.id 
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
     @tweet.save
     redirect_to tweets_path
   end
@@ -46,6 +45,6 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:body) 
+    params.require(:tweet).permit(:body)
   end
 end
